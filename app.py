07f7158c97577cb99b9f3ec4474689928995cff9
FaerieVearth <valentin.0.gregor@gmail.com>
2020-05-13 23:54:15 +0200

    removed check

--- app.py
+++ app.py
@@ -8,5 +8,4 @@ def index():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()  # if you use the port parameter, delete it before deploying to Heroku
+app.run()  # if you use the port parameter, delete it before deploying to Heroku
